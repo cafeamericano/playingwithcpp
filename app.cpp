4da438870188279e0ac5a4f9bc1d9ae43f5d2d9d
Matthew <mfarmer5102@gmail.com>
2019-09-05 14:29:57 -0400

    DRY improvements

--- app.cpp
+++ app.cpp
@@ -1,12 +1,38 @@
+//SETUP
 #include <iostream>
+#include <cmath>
 using namespace std;
 
+//FUNCTIONS
+void printLineBreak(int lineCount) {
+    for (int i = 0; i < lineCount; i++){
+        cout << "\n";
+    }
+}
+
+//MAIN
 int main() {
-    string nameVar;
-    cout << "What's your name?: ";
-    cin >> nameVar;
     
-    cout << "You said: " << nameVar;
-    cout << "\n";
+    //Add in some spacing
+    printLineBreak(1);
+
+    //Variable initialization
+    float pounds, inches;
+    
+    //Find out user's weight in pounds
+    cout << "What is your weight in pounds?: ";
+    cin >> pounds;
+    
+    //Find out user's height in inches
+    cout << "What is your height in inches?: ";
+    cin >> inches;
+    
+    //Display BMI calculation to user
+    cout << "Your BMI is: ";
+    cout << (703 * pounds)/(inches * inches);
+    
+    //End program
+    printLineBreak(2);
     return 0;
+    
 }
